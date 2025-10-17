2ea07244422cf443358d1a16ea3d8fcd88ffabe7
boxdate <kenji.matsuki@outlook.com>
2025-10-17 12:00:55 +0900

    データの永続化を追加

--- BlazorToDo/ToDoApp/obj/Debug/net8.0/ToDoApp.AssemblyInfo.cs
+++ BlazorToDo/ToDoApp/obj/Debug/net8.0/ToDoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+523a77225fb1b45225fe316a3afdcf33d196a4df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6acda22ef5dbe42b757cbb8ebea2a7925833927b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
